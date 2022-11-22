eebf9c5c9ce2e48ef9f710f4637842d6714f1def
Jana <90056190+nesti13@users.noreply.github.com>
2022-11-22 18:43:50 +0300

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        public static Double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static Double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static Double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static Double Divide(double x, double y)
+        {
+            if (y != 0)
+            {
+                return x / y;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+}
